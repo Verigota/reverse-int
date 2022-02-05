b9da5f1909828d4a3fe38a3f530c52ea0b5d761f
Tatiana <amora4ka@gmail.com>
2022-02-05 17:47:30 +0300

    feat: complite reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const numToString = Math.abs(n).toString();
+    const strToArray = Array.from(numToString);
+    return Number(strToArray.reverse().join(''));
+    
 }
+
+
